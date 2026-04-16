22dd8c13c31a2ef84c732a5f3f3b11dd1393e593
akarthikbunny <arigekarthik18@gmail.com>
2026-04-16 14:47:53 +0530

    wrote greet funcn to understand stash

---
+++ ex_3_Stash_stimulation.js
@@ -0,0 +1,16 @@
+// git stash — Pause your work safely -save work temporarily without committing it, and come back to it later.
+// You’re working on a feature, but suddenly need to switch branches.
+
+
+// app.js ,code before stash , not yet commited 
+// if u type --> git checkout main
+
+// Git says:
+
+// "You have uncommitted changes" -->solution : stash it -->git stash 
+// Your changes are saved temporarily
+// Your working directory becomes clean
+function greet() {
+  console.log("Hello World");
+}
+
